292b18f240f0981e0334797f5ff3a9578b54587c
Guilhermo Masid <guilhermomasid@gmail.com>
2025-05-19 19:15:50 -0300

    resolve domain to address

--- src/main.c
+++ src/main.c
@@ -1,21 +1,31 @@
-#include <errno.h>
-#include <netinet/in.h>
-#include <netinet/ip_icmp.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/socket.h>
-#include <unistd.h>
+#include <string.h>
 
-int main() {
-  int sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+int main(int argc, char **argv) {
+  struct addrinfo hints;
+  struct addrinfo *result;
+  char ipstr[INET_ADDRSTRLEN];
 
-  if (sockfd < 0) {
-    printf("You probably need to run this as root (try sudo).\n");
+  if (argc != 2) {
+    printf("Usage: %s <hostname> or ipv4\n", argv[0]);
     return EXIT_FAILURE;
   }
 
-  printf("Raw socket created successfully! (fd = %d)\n", sockfd);
+  bzero(&hints, sizeof(hints));
+  hints.ai_family = AF_INET;  // IPv4
 
-  close(sockfd);
+  if (getaddrinfo(argv[1], NULL, &hints, &result) != 0) {
+    perror("getaddrinfo");
+    return EXIT_FAILURE;
+  }
+  struct sockaddr_in *ipv4 = (struct sockaddr_in *)result->ai_addr;
+  inet_ntop(AF_INET, &(ipv4->sin_addr), ipstr, sizeof(ipstr));
+
+  printf("Resolved %s to %s\n", argv[1], ipstr);
+  freeaddrinfo(result);  // Free the linked list
+
   return EXIT_SUCCESS;
 }
