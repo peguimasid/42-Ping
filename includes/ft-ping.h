6c40cf3d66f7770dbc3e7472d84b0211ca2641ea
Guilhermo Masid <guilhermomasid@gmail.com>
2025-06-04 19:11:46 -0300

    check fot errors

---
+++ includes/ft-ping.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <arpa/inet.h>
+#include <math.h>
+#include <netdb.h>
+#include <netinet/ip_icmp.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
