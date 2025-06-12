1d99e6efa8960be86c1d52207e48ff80aea80655
Guilhermo Masid <guilhermomasid@gmail.com>
2025-06-12 19:36:08 -0300

    some changes

--- src/ping.c
+++ src/ping.c
@@ -1,5 +1,5 @@
 #include "../includes/ft-ping.h"
 
 void ping() {
-  printf("PING %s (%s) %d(%ld) bytes of data.\n", ctx->target, ctx->target_ip, PAYLOAD_SIZE, PACKET_SIZE);
+  printf("PING %s (%s) %d(%d) bytes of data.\n", ctx->target, ctx->target_ip, PAYLOAD_SIZE, PACKET_SIZE);
 }
